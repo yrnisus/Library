322fef99dd885f3b46fe4e9869c8bbb4ec6ab602
yrnisus <cjmckeefry@Yahoo.com>
2022-06-01 14:12:48 -0400

    Modal JS

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function Book(title, author, numOfPages, haveRead) {
 function addBookToLibrary() {
   // do stuff here
 }
+
+
+const modalBtn = document.querySelector('.modal-btn');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.close-modal-btn');
+
+
+modalBtn.addEventListener('click', function() {
+  modalBg.classList.add('bg-active');
+})
+
+modalBg.addEventListener('click', function() {
+  if(modalBg.classList.contains('bg-active'))
+  modalBg.classList.remove('bg-active');
+})
+
+modalClose.addEventListener('click', function() {
+  modalBg.classList.remove('bg-active');
+})
