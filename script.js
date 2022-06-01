ffbd01c54f92a1b7f60270622e0f05112d41f580
yrnisus <cjmckeefry@Yahoo.com>
2022-06-01 12:59:15 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, numOfPages, haveRead) {
+ this.title = title;
+ this.author = author;
+ this.numOfPages = numOfPages;
+ this.haveRead = haveRead;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
